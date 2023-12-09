eb6f1d7b5d2860f8242f647eeadbb8fcde06ecc4
papered <papered@paperedui-MacBookAir.local>
2023-12-09 21:18:05 +0900

    왜안됨

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://dev-util.io',
-    generateRobotsTxt: true, // (optional)
+    generateRobotsTxt: true,
     // ...other options
   }
